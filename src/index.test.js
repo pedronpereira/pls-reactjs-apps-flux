8c35f10b449bc2872c96c74ee5c193fc64472164
PedroNunoP <pedronprpereira@gmail.com>
2019-01-06 20:32:20 +0000

    implemented mock api and mock data generation

--- src/index.test.js
+++ src/index.test.js
@@ -9,13 +9,12 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () => {
-    it('should say hello', (done) =>{
+    it('should say hello', () =>{
         const index = fs.readFileSync('./src/index.html', "utf-8");
         const { JSDOM } = jsdom;
         const dom = new JSDOM(index);
-        const h1 = dom.window.document.getElementsByTagName('h1')[0];
-        expect(h1.innerHTML).to.equal("Hello World!");
-        done();
+        const h1 = dom.window.document.getElementsByTagName("h1")[0];
+        expect(h1.innerHTML).to.equal("Users");
         dom.window.close();
         });
 });
